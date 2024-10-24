777f2867dc7780c37bb2fb189be4b003c4b01d96
unknown <prantokih42@gmail.com>
2024-10-25 01:30:38 +0600

    axiosSecure token added

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -4,7 +4,22 @@ const axiosSecure = axios.create({
     baseURL:'http://localhost:5500'
 })
 const useAxiosSecure = () => {
+   axiosSecure.interceptors.request.use(function(config){
+    const token = localStorage.getItem('access-token');
+    config.headers.authorization = `Bearer ${token}`
+    return config;
+   },function(error){
+       return Promise.reject(error);
+   });
+   axiosSecure.interceptors.response.use(function(response){
+       return response;
+   }),function(error){
 
+
+    if(error.response.status === 401 || error.response.status === 403){
+    return Promise.reject(error);
+       
+   } }
    return axiosSecure ;
 };
 
