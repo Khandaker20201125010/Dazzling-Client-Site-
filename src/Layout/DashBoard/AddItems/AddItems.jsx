3dcb333becc2d3a512d9fea17a29957018be6449
unknown <prantokih42@gmail.com>
2024-10-29 23:04:56 +0600

    base table added

--- src/Layout/DashBoard/AddItems/AddItems.jsx
+++ src/Layout/DashBoard/AddItems/AddItems.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React from "react";
+import SectionTitle from "../../../Componenets/SectionTitle/SectionTitle";
+import { useForm } from "react-hook-form";
 
 const AddItems = () => {
-    return (
-        <div>
-            todo add items
-            
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <SectionTitle
+        subHeading="Add A New Item"
+        heading="Add Items"
+      ></SectionTitle>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("name")} />
+          <select {...register("category")} className="select select-success w-full max-w-xs">
+            <option disabled selected>
+              Select a category
+            </option>
+            <option value="popular">Popular</option>
+            <option value="famous">Famous</option>
+            <option value="jackets">Jackets</option>
+            <option value="shoes">Shoes</option>
+            <option value="bags">Bags</option>
+            <option value="hats">Hats</option>
+            <option value="discounts">Discounts</option>
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddItems;
