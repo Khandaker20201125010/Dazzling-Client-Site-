87fa19368c465ca743d05017c96bce31b158d35f
unknown <prantokih42@gmail.com>
2024-11-03 22:01:06 +0600

    getting back the commit

--- src/Componenets/Carts/MenuCarts.jsx
+++ src/Componenets/Carts/MenuCarts.jsx
@@ -12,8 +12,7 @@ const MenuCarts = ({ cart ,refetch}) => {
             title: "Are you sure?",
             text: "You won't be able to revert this!",
             icon: "warning",
-            showCancelButton: true,
-             
+            showCancelButton: true,          
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
             confirmButtonText: "Yes, delete it!"
