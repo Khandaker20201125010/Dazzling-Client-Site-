5441df6c1eb41e9e3e5f5b6ebb53ba5e7b47bd19
unknown <prantokih42@gmail.com>
2024-11-09 23:26:24 +0600

    footer logo addded

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,73 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import icon from "../../../../public/lago.png";
 const Footer = () => {
-    return (
-        <div className='mt-10'>
-            <footer className="bg-gray-900 text-white py-8">
-                <div className="container mx-auto flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-52">
-                    <div className="text-center md:text-left">
-                       <Link to='/Contacts'> <button className="text-xl font-bold mb-2 mx-16 ">Contact Us</button></Link>
-                        <p className='mx-10' > Feel free to contact us</p>
-                        <p className='mx-12'>+88 123456789</p>
-                        <p className='mx-8'>Mon - Fri: 08:00 - 22:00</p>
-                        <p className='mx-8'>Sat - Sun: 10:00 - 23:00</p>
-                    </div>
-                    <div className="text-center md:text-left ">
-                        <h3 className="text-xl font-bold mb-2">Follow Us</h3>
-                        <p className='mt-4'>Join us on social media</p>
-                        <div className="grid grid-flow-col gap-2 mt-5 ">
-                            <a>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    className="fill-current">
-                                    <path
-                                        d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                                </svg>
-                            </a>
-                            <a>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    className="fill-current">
-                                    <path
-                                        d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                                </svg>
-                            </a>
-                            <a>
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    className="fill-current">
-                                    <path
-                                        d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                                </svg>
-                            </a>
-                        </div>
-                        <div>
-
-                        </div>
-                    </div>
-                </div>
-            </footer>
-            <div>
-                <footer className="footer footer-center bg-yellow-300 text-base-content p-4">
-                    <aside>
-                        <p className='text-black'>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
-                    </aside>
-                </footer>
+  return (
+    <div className="mt-10">
+      <footer className="bg-gray-900 text-white py-8">
+        <div className="container mx-auto flex flex-col md:flex-row justify-center items-center space-y-4 md:space-y-0 md:space-x-52">
+          <div className="bg-transparent">
+            <img className="w-20 h-20  rounded-full" src={icon} alt="" />
+            <h3>About Us</h3>
+          </div>
+          <div className="text-center md:text-left">
+            <Link to="/Contacts">
+              {" "}
+              <button className="text-xl font-bold mb-2 mx-16 ">
+                Contact Us
+              </button>
+            </Link>
+            <p className="mx-10"> Feel free to contact us</p>
+            <p className="mx-12">+88 123456789</p>
+            <p className="mx-8">Mon - Fri: 08:00 - 22:00</p>
+            <p className="mx-8">Sat - Sun: 10:00 - 23:00</p>
+          </div>
+          <div className="text-center md:text-left ">
+            <h3 className="text-xl font-bold mb-2">Follow Us</h3>
+            <p className="mt-4">Join us on social media</p>
+            <div className="grid grid-flow-col gap-2 mt-5 ">
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                </svg>
+              </a>
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                </svg>
+              </a>
+              <a>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="fill-current"
+                >
+                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                </svg>
+              </a>
             </div>
+            <div></div>
+          </div>
         </div>
-
-
-    );
+      </footer>
+      <div>
+        <footer className="footer footer-center bg-yellow-300 text-base-content p-4">
+          <aside>
+            <p className="text-black">
+              Copyright © {new Date().getFullYear()} - All right reserved by
+              ACME Industries Ltd
+            </p>
+          </aside>
+        </footer>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
