8f2574c984a1676052a14ac3974f14219730ad42
unknown <prantokih42@gmail.com>
2024-11-29 15:19:40 +0600

    design modified

--- src/Pages/Shared/ProductItem/ProductItem.jsx
+++ src/Pages/Shared/ProductItem/ProductItem.jsx
@@ -3,27 +3,25 @@ import { Link } from "react-router-dom";
 
 const ProductItem = ({ product }) => {
   const { name, image, price, description } = product;
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth", // Smooth scroll animation
-  });
 
   return (
-    <div data-aos="zoom-in" className="md:flex items-center justify-between p-4 bg-slate-800 rounded-md ">
+    <div
+      data-aos="zoom-in"
+      className="md:flex items-center justify-between p-4 bg-slate-800 rounded-md"
+    >
       <div className="flex gap-5">
         <div className="image-container">
           <img className="product-image" src={image} alt={name} />
         </div>
         <div>
-          <h3 className="uppercase  font-bold">{name}</h3>
-          <p className="w-3/4">{description}</p>
+          <h3 className="uppercase font-bold text-white">{name}</h3>
+          <p className="w-3/4 text-gray-300">{description}</p>
         </div>
       </div>
-      <div className="flex flex-col gap-5 max-sm:mt-12">
-        <p className="text-lg text-lighting  text-white">{price}TK</p>
+      <div className="flex flex-col gap-5 max-sm:mt-12 max-sm:text-end max-sm:px-10">
+        <p className="text-lg text-lighting text-white">{price} TK</p>
 
         <Link to={`/details/${product?._id}`}>
-          {" "}
           <button
             className="buton"
             onClick={() =>
@@ -38,7 +36,7 @@ const ProductItem = ({ product }) => {
             <div className="left"></div>
             <div className="bottom"></div>
             <div className="right"></div>
-          </button>{" "}
+          </button>
         </Link>
       </div>
     </div>
